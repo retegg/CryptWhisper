4b588254cca792592738b12db9df503f979752f4
retegg <arkipiqueras2018@gmail.com>
2023-06-21 15:45:36 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def tunnel():
                 msg = msg.encode("utf-8")
                 ciphertext = cipher.encrypt(msg)
                 response = requests.get(f"{url}/lis?data={ciphertext.hex()}", proxies=proxies)
-                requests.get(f"{url}/conn?url={url_tunnel}", stream=True, proxies=proxies)
+                requests.get(f"{url_tunnel}/conn?url={url}", stream=True, proxies=proxies)
                 return "ok!"
             else:
                 return "No URL provided."
